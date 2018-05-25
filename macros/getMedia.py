ffe927cce3d5bc4de07cf3749d06706575c24f21
a.succurro-donquijote <a.succurro@gmail.com>
2018-05-25 17:49:47 +0200

    Done media db retrieve

--- macros/getMedia.py
+++ macros/getMedia.py
@@ -14,6 +14,10 @@ def main():
     verbose = args.verbose
     # sbp = subprocess.Popen(("grep", "-w", "%s"%args.bmname, "%s"%args.rxnstsv), stdout = subprocess.PIPE)
     # l = sbp.communicate()[0]
+    mf = open('/tmp/matched.log','w')
+    mmf = open('/tmp/multimatched.log','w')
+    nmf = open('/tmp/notmatched.log','w')
+    wells = []
     with open(args.mediafile,'r') as infile:
         for line in infile:
             l = line.split('\t')
@@ -22,33 +26,53 @@ def main():
                 continue
             n = n.replace(args.compoundname+'-', '')
             c = l[5:-1]
-            sbp = subprocess.Popen(("grep", "-i", "%s"%n, "../rhizobiumRoot491/data/biolog_summary.csv"), stdout = subprocess.PIPE)
+            sbp = subprocess.Popen(("grep", "-i", "%s"%n, "%s"%args.biolog), stdout = subprocess.PIPE)
             match = (sbp.communicate()[0]).decode('ascii')
             #print(len(match))
             if not match:
-                print('***** \t', n, ' not found')
+                print('?', n, ' was not matched')
+                nmf.write(n+'\n')
             else:
                 mm = match.split('\n')
-                if len(mm)>2:
-                    print('***** \t', n, ' has more matches!')
-                    for m in range(len(mm)-1):
-                        x = mm[m].split('\t')
-                        print('***** \t \t', n, x[1], x[0])
-                    continue
+                # if len(mm)>2:
+                #     mmf.write(n+'\n')
+                #     for m in range(len(mm)-1):
+                #         x = mm[m].split('\t')
+                #         mmf.write('>'+x[0]+'\t'+n+'\t'+x[1]+'\n')
+                #     continue
                 for m in range(len(mm)-1):
                     x = mm[m].split('\t')
-                    print(n, x[1], x[0])
+                    if n != x[1]:
+                        print('!', n, ' was also matched with ', x[1], x[0], '; this will be skipped')
+                        mmf.write('ls '+args.outpath+x[0]+'_'+x[1]+'.tsv\n')
+                        continue
                     with open(args.outpath+x[0]+'_'+n+'.tsv','w') as outfile:
                         outfile.write('ID\tminFlux\tmaxFlux\tconcentration\n')
                         for cpd in c:
                             outfile.write(cpd.replace(';','\t')+'\n')
+                    mf.write(x[0]+'\t'+n+'\n')
+                    wells.append(x[0])
+    #wells.sort()
+    mf.close()
+    mmf.close()
+    nmf.close()
 
-                    #print(n, x)
-
-
+    nfw=[]
     #subs = map(lambda x: x[x.index('cpd'):x.index('cpd')+8], s)
     #prods = map(lambda x: x[x.index('cpd'):x.index('cpd')+8], p)
-
+    print('X The following metabolites were not found in the DB (missing ID?)')
+    with open(args.biolog,'r') as infile:
+        for line in infile:
+            l = line.split('\t')
+            if l[0] == 'Well':
+                continue
+            if l[0] not in wells:
+                print('\t',l[0], l[1])
+                nfw.append(l[0])
+    aw = nfw+wells
+    aw.sort()
+    nfw.sort()
+    #print('\n'.join(aw))
     #with open(args.outpath+'biomass.m','w') as outfile:
     #    outfile.write('function [s, p] = biomass()\n')
     #    outfile.write('s = {\''+'\', \''.join(subs)+'\'};\n')
@@ -62,7 +86,8 @@ def options():
     parser.add_argument('-V', '--verbose', help='increase output verbosity', action='store_true')
     parser.add_argument('-m', '--mediafile', help='tsv file with media definition', default='../data/ModelSEEDdata/media_list_with_meta.tsv')
     parser.add_argument('-c', '--compoundname', help='name of source compound', default='Nitrogen')
-    parser.add_argument('-o', '--outpath', help='path for output file', default='../rhizobiumRoot491/data/singleNMedia/')
+    parser.add_argument('-b', '--biolog', help='name of biolog tsv file', default='../rhizobiumRoot491/data/biolog_summary.csv')
+    parser.add_argument('-o', '--outpath', help='path for output file', default='../rhizobiumRoot491/media/singleNMedia/')
     args = parser.parse_args()
     if args.verbose:
         print("verbosity turned on")
