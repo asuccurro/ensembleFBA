904624be87426424dfed37795ad97b8080ee5101
a.succurro <succurro@ad.hhu.de>
2018-05-29 19:36:26 +0200

    Amended biomass function to avoid missing compounds; not sure this is legit (mass/charge balance)

--- runmatlab/startup.m
+++ runmatlab/startup.m
@@ -1,4 +1,5 @@
 addpath(genpath('../data/'));
 addpath(genpath('../bin/'));
 addpath(genpath('../rhizobiumRoot491/'));
+addpath(genpath('../macros/'));
 addpath('/home/succurro/Downloads/packages/gurobi652/linux64/matlab/')
---
+++ runmatlab/test-getGSMNMGrowthConditions.m
@@ -0,0 +1,2 @@
+load seed_rxns
+d = getGSMNMGrowthConditions(seed_rxns_mat, '../rhizobiumRoot491/data/growthMatrix_Root491.csv')
