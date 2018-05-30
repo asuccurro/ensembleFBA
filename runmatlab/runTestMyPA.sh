e57b8182b5107f2e649c9e97603c44ccea27b06c
a.succurro <succurro@ad.hhu.de>
2018-05-30 20:11:49 +0200

    Test scripts

---
+++ runmatlab/runTestMyPA.sh
@@ -0,0 +1,1 @@
+nohup matlab -nodesktop -nosplash -nodisplay -r "testMyPA" > /tmp/runTestMyPA.log & 
---
+++ runmatlab/runTestPA.sh
@@ -0,0 +1,1 @@
+nohup matlab -nodesktop -nosplash -nodisplay -r "testPA;exit" > /tmp/testPA.log 2>/tmp/err.out &
