7ea8b1fe6b4339ed13f96315a8e38688525dcfcf
a.succurro-donquijote <a.succurro@gmail.com>
2018-06-01 18:29:03 +0200

    Fix bash script

--- runmatlab/runGapfill.sh
+++ runmatlab/runGapfill.sh
@@ -1,3 +1,3 @@
 nohup matlab -nodesktop -nosplash -nodisplay -r "gapfill;exit" > /tmp/gapfill.log 2>/tmp/err.out &
 mkdir -p ../outputs/gapfill_sequential
-mv Gapfill* ../outputs/gapfill_sequential/
+mv GapFill_Sequence* ../outputs/gapfill_sequential/
