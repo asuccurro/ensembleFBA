70bde326b76c012f3a5cf3216eb9c35db3858170
a.succurro <succurro@ad.hhu.de>
2018-06-05 11:52:17 +0200

    Bash rename

--- runmatlab/runEnsemble.sh
+++ runmatlab/runEnsemble.sh
@@ -1,3 +1,3 @@
 matlab -nodesktop -nosplash -nodisplay -r "ensemble;exit" > /tmp/ensemble.log 2>/tmp/err.out 
 mkdir -p ../outputs/ensemble/
-mv Ensemble* ../outputs/ensemble/
+mv ensemble* ../outputs/ensemble/
