9540e8c153cab6b0bbde031078b3d71b1451333f
a.succurro <succurro@ad.hhu.de>
2018-06-12 20:45:55 +0200

    Running after bugfix

---
+++ runmatlab/multiEnsemble2.sh
@@ -0,0 +1,17 @@
+for c in exc5N_fixed ; do
+	mkdir -p ../outputs/root491/${c}
+	for s in 0 1; do
+		for n in 21; do
+			for t in 0; do
+				fnm=ensemble_${c}_${s}_${n}_${t}
+				cp ../macros/00_ensemble.m ${fnm}.m
+				sed -i "s/XXXTEST/${t}/g" ${fnm}.m
+				sed -i "s/XXXSTOC/${s}/g" ${fnm}.m
+				sed -i "s/XXXSIZE/${n}/g" ${fnm}.m
+				sed -i "s/_XXXCOND//g" ${fnm}.m
+				sed -i "s/XXXCOND/${c}/g" ${fnm}.m
+				nohup matlab -nodesktop -nosplash -nodisplay -r "${fnm};exit" > /tmp/${fnm}.log 2>/tmp/err_${fnm}.out &
+			done
+		done
+	done
+done
