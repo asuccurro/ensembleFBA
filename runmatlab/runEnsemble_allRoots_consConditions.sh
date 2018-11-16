68c2a5cf9dfd332e41f1f696bcc879aab32caf97
a.succurro-ulysses <a.succurro@gmail.com>
2018-11-16 16:07:17 +0100

    Fixed script to run the ensembles

--- runmatlab/runEnsemble_allRoots_consConditions.sh
+++ runmatlab/runEnsemble_allRoots_consConditions.sh
@@ -2,7 +2,7 @@ ngc=10
 gc=21
 for o in Root9 Root491 Root66D1; do
     #for c in "" _exclude_A2-A5-A12-B6-B10; do
-    for c in _exclude_C4-C9-D2-D6-D7-D10-E6-E7-E10-G4-G6-G7-G10-G12 _exclude_A2-A5-A12-B6-B10; do
+    for c in _exclude_not_found_and_G12 _exclude_A2-A5-A12-B6-B10; do
 	mkdir -p ../outputs/${o}${c}
 	for s in 1; do
 	    for n in 50; do
